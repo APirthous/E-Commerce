0330bd661fdced669d9540c7fdee9558d2d5b108
APirthous <85134940+APirthous@users.noreply.github.com>
2021-06-07 12:47:02 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'HP Laptop',
+        price: 49000,
+        image: 'https://picsum.photos/id/2/2400/1600',
+      },
+      {
+        id: '2',
+        name: 'Camera',
+        price: 4999,
+        image:'https://picsum.photos/id/250/2400/1600',
+      },
+      {
+        id: '3',
+        name: 'Coffee Mug',
+        price: 250,
+        image: 'https://picsum.photos/id/30/2400/1600',
+      },
+      {
+        id: '4',
+        name: 'Fork',
+        price: 150,
+        image: 'https://picsum.photos/id/23/2400/1600',
+      },
+      
+    ],
+  };
+  export default data;
+  
